ccd00a8a4a74f738d675725e97d31032a63b1e35
r12f <r12f.code@gmail.com>
2015-08-30 13:15:48 -0700

    Adding powershell profile.

--- install.cmd
+++ install.cmd
@@ -2,6 +2,7 @@
 
 set CONFIG_ROOT=%~dp0.
 set CMD_CONFIG_ROOT=%CONFIG_ROOT%\cmd
+set POWERSHELL_CONFIG_ROOT=%CONFIG_ROOT%\powershell
 set VIM_CONFIG_ROOT=%CONFIG_ROOT%\vimrc
 
 echo Install configuration ...
@@ -9,10 +10,71 @@ echo Install configuration ...
 :: Install command prompt configuration
 echo Install command prompt configuration ...
 reg add "HKLM\Software\Microsoft\Command Processor" /v "AutoRun" /t REG_SZ /d "%CMD_CONFIG_ROOT%\cmd-init.cmd" /f > nul
-echo.
+
+:: Install powershell configuration
+echo Install powershell prompt configuration ...
+if not exist "%USERPROFILE%\Documents\WindowsPowerShell" (
+    mkdir "%USERPROFILE%\Documents\WindowsPowerShell"
+)
+call :CreateSymbolLinkWithBackup %POWERSHELL_CONFIG_ROOT%\profile.ps1 %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 
 :: Install VIM configuration
-call %VIM_CONFIG_ROOT%\install.cmd
 echo.
+call %VIM_CONFIG_ROOT%\install.cmd
 
+:: Complete
+echo.
 echo Configuration installation complete!
+exit /b 0
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Function: CreateSymbolLinkWithBackup
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:CreateSymbolLinkWithBackup
+set source_file=%1
+set dest_file=%2
+
+call :CreateBackupFileIfNeeded
+if not errorlevel 0 (
+    exit /b 1
+)
+
+echo Creating symbol link: "%dest_file%" -^> "%source_file%"
+if exist %source_file%\ (
+    mklink /D "%dest_file%" "%source_file%"
+) else (
+    mklink "%dest_file%" "%source_file%"
+)
+if not errorlevel 0 (
+    echo Create backup failed.
+    exit /b 1
+)
+
+exit /b 0
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Function: CreateBackupFileIfNeeded
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:CreateBackupFileIfNeeded
+if not exist "%dest_file%" (
+    exit /b 0
+)
+
+set backup_id=0
+set backup_file=%dest_file%.bak
+
+:CheckBackupFileExist
+if exist "%backup_file%" (
+    set /a backup_id+=1
+    set backup_file=%dest_file%.%backup_id%.bak
+    goto :CheckBackupFileExist
+)
+
+echo Backing up "%dest_file%" to "%backup_file%" ...
+move %dest_file% %backup_file% >nul 2>&1
+if not errorlevel 0 (
+    echo Create backup failed.
+    exit /b 1
+)
+
+exit /b 0
---
+++ powershell/profile.ps1
@@ -0,0 +1,70 @@
+################################################################
+#
+# r12f's powershell init script (https#//github.com/r12f/config)
+# email# r12f.code@gmail.com
+#
+# This init script contains several sections#
+# 0. Environment settings
+# 1. Aliases
+#
+################################################################
+
+################################################################
+#
+# 0. Environment settings
+#
+################################################################
+$env:CONFIG_ROOT = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$env:MY_EDITOR = "gvim"
+
+function Prompt
+{
+    # Print the current time:
+    Write-Host ("[") -nonewline -foregroundcolor DarkGray
+    Write-Host (Get-Date -format HH:mm:ss) -nonewline -foregroundcolor Gray
+    Write-Host ("] ") -nonewline -foregroundcolor DarkGray
+
+    # Username
+    Write-Host "$env:USERNAME" -ForegroundColor DarkCyan -NoNewline
+    Write-Host "@" -NoNewLine
+
+    # Machine
+    Write-Host "$env:COMPUTERNAME" -ForegroundColor DarkGreen -NoNewLine
+    Write-Host ":" -ForegroundColor DarkGreen -NoNewLine
+
+    # Directory
+    Write-Host "$(Get-Location)" -ForegroundColor DarkYellow
+    Write-Host ">" -NoNewLine
+
+    return " ";
+}
+
+################################################################
+#
+# 1. Aliases
+#
+################################################################
+function CallAlias-e { if ($args -NE 0) { start $env:MY_EDITOR $args } else { start $env:MY_EDITOR } }
+Set-Alias e CallAlias-e
+function CallAlias-f { if ($args -NE 0) { start start $args } else { start . } }
+Set-Alias f CallAlias-f
+function CallAlias-.. { cd .. }
+Set-Alias .. CallAlias-..
+function CallAlias-... { cd ..\.. }
+Set-Alias ... CallAlias-...
+function CallAlias-.2 { cd ..\.. }
+Set-Alias .2 CallAlias-.2
+function CallAlias-.3 { cd ..\..\.. }
+Set-Alias .3 CallAlias-.3
+function CallAlias-.4 { cd ..\..\..\.. }
+Set-Alias .4 CallAlias-.4
+function CallAlias-.5 { cd ..\..\..\..\.. }
+Set-Alias .5 CallAlias-.5
+function CallAlias-.6 { cd ..\..\..\..\..\.. }
+Set-Alias .6 CallAlias-.6
+function CallAlias-~ { cd /d $env:USERPROFILE }
+Set-Alias ~ CallAlias-~
+function CallAlias-l { dir -Force $args }
+Set-Alias l CallAlias-l
+function CallAlias-la { dir -Force  $args }
+Set-Alias la CallAlias-la
